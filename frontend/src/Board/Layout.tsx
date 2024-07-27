762cc243bd7cb4a462cab037b42fc9147a786fd7
Rakesh <rakeshgandla200@gmail.com>
2024-07-27 19:42:20 +0530

    seperated backend code from main

--- frontend/src/Board/Layout.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react"
-
-function Layout({ children }: { children: React.ReactNode }) {
-    return <div className="bg-purple h-[100vh] text-white flex flex-row justify-center items-center">{children}</div>
-}
-
-export default Layout
