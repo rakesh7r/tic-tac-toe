b83de448a2720016d2c44c7c3749c31f50a68680
Rakesh <rakeshgandla200@gmail.com>
2024-07-27 20:04:24 +0530

    added http server to the backend

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,8 +1,17 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const http_1 = __importDefault(require("http"));
 const ws_1 = require("ws");
 const GameHandler_1 = require("./GameHandler");
-const wss = new ws_1.WebSocketServer({ port: 8080 });
+const cors_1 = __importDefault(require("cors"));
+const app = (0, express_1.default)();
+app.use((0, cors_1.default)());
+const server = http_1.default.createServer(app);
+const wss = new ws_1.Server({ server });
 const gameHandler = new GameHandler_1.GameHandler();
 wss.on("connection", (ws) => {
     ws.on("error", console.error);
@@ -12,4 +21,9 @@ wss.on("connection", (ws) => {
         console.log("Connection closed");
         gameHandler.removeUser(ws);
     });
+});
+// Start the server
+const PORT = process.env.PORT || 8080;
+server.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
 });
