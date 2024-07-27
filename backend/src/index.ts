0364e9824268d2048d90e81230a6cb9680bee659
Rakesh <rakeshgandla200@gmail.com>
2024-07-27 23:34:38 +0530

    added http server to the backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,13 @@
-import { WebSocketServer, WebSocket } from "ws"
+import express from "express"
+import http from "http"
+import WebSocket, { Server as WebSocketServer } from "ws"
 import { GameHandler } from "./GameHandler"
-import { userActions } from "./actions.const"
+import cors from "cors"
 
-const wss = new WebSocketServer({ port: 8080 })
+const app = express()
+app.use(cors())
+const server = http.createServer(app)
+const wss = new WebSocketServer({ server })
 const gameHandler = new GameHandler()
 
 wss.on("connection", (ws: WebSocket) => {
@@ -13,4 +18,10 @@ wss.on("connection", (ws: WebSocket) => {
         console.log("Connection closed")
         gameHandler.removeUser(ws)
     })
+})
+
+// Start the server
+const PORT = process.env.PORT || 8080
+server.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`)
 })
