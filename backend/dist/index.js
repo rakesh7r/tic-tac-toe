aa2aa2127460b3111f9cb3191a9b4a1a5b76b610
Rakesh <rakeshgandla200@gmail.com>
2024-07-28 14:01:25 +0530

    port changed to 80

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -26,7 +26,7 @@ app.get("/", (req, res) => {
     res.send("App is up and running");
 });
 // Start the server
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 80;
 server.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
 });
